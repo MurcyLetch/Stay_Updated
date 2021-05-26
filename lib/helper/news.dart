e1ebcdaf431811413ab5dd1dc50334bd9629a2cb
MurcyLetch <arijitindu99@gmail.com>
2021-05-26 22:01:28 +0530

    json data fetch from api

---
+++ lib/helper/news.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:stay_updated/models/article_model.dart';
+import 'package:http/http.dart' as http;
+class News{
+  List<Articlemodel> news=[];
+  Future<void>getnews() async{
+    String url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e1fa5c9782714e3cbdfccd8bf6b07d7e";
+    var response=await http.get(Uri.parse(url));  //see once more
+
+    var jsondata =jsonDecode(response.body);
+    if(jsondata['status']=="ok"){
+      jsondata['articles'].forEach((element){
+        if(element["urlToimage"]!=null && element["description"]!=null){
+          Articlemodel articlemodel=Articlemodel(
+            title: element['title'],
+            author:  element['author'],
+            description:  element['description'],
+            url:  element['url'],
+            urlToImage:  element['urlToImage'],
+            content:  element['content'],
+            publishedAt: element['publishedAt']
+          );
+          news.add(articlemodel);
+        }
+
+      });
+    }
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:stay_updated/helper/data.dart';
+import 'package:stay_updated/helper/news.dart';
+import 'package:stay_updated/models/article_model.dart';
+import 'package:stay_updated/models/category_model.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +10,24 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  // ignore: deprecated_member_use
+  List<Categorymodel>categories=new List<Categorymodel>();
+  // ignore: deprecated_member_use
+  List<Articlemodel>articles=new List<Articlemodel>();
+  bool _loading=true;
+  @override
+  void initState() { 
+    super.initState();
+    categories=getcatagories();
+  }
+  getnews() async {
+    News newsClass=News();
+    await newsClass.getnews();
+    articles=newsClass.news;
+    setState(() {
+          _loading=false;
+        });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,8 +44,37 @@ class _HomeState extends State<Home> {
         ),
         elevation: 0.0,
       ),
-      body: Container(
-        child:  ,
+      body: _loading ? Center(
+        child: Container(
+          child:CircularProgressIndicator(),
+        ),
+      ) : Container(
+        child: Column(
+          children: [
+
+            ////categories
+
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              height: 70,
+              child: ListView.builder(
+                itemCount: categories.length,
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context,index){
+                  return Categorytile(
+                    imageurl: categories[index].imageurl,
+                    categoryname: categories[index].categoryname,
+                  );
+                }
+                ),
+            )
+
+            ////blogs
+
+
+          ],
+          ) ,
       ),
     );
   }
@@ -34,10 +85,54 @@ class Categorytile extends StatelessWidget {
  Categorytile({this.imageurl,this.categoryname});
  @override
   Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+
+      },
+      child: Container(
+        margin:EdgeInsets.only(right: 16),
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(6),
+              child: Image.network(imageurl,width: 120,height:60,fit:BoxFit.cover)
+              ),
+            Container(
+              alignment: Alignment.center,
+              width:120,height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                color:Colors.black26,
+              ),
+                child: Text(categoryname,style: 
+                TextStyle(
+                  color:Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  ),
+                ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Blogtile extends StatelessWidget {
+
+  final String imageurl,title,desc;
+
+  Blogtile({@required this.imageurl,@required this.title,@required this.desc});
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Stack(
+      child: Column(
         children: [
-          Image.network(imageurl,width: 120,height:60),
+          Image.network(imageurl),
+          Text(title),
+          Text(desc),
         ],
       ),
     );
