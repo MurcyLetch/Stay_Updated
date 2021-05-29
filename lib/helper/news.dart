61e5719558c3511e7f3af8ffa1a60bac8e661499
MurcyLetch <arijitindu99@gmail.com>
2021-05-29 14:19:12 +0530

    ..

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -6,12 +6,15 @@ class News{
   List<Articlemodel> news=[];
   Future<void>getnews() async{
     String url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e1fa5c9782714e3cbdfccd8bf6b07d7e";
-    var response=await http.get(url);  //see once more
+    var response=await http.get(Uri.parse(url));  //see once more
 
     var jsondata =jsonDecode(response.body);
     if(jsondata['status']=="ok"){
+     // print(jsondata);
       jsondata["articles"].forEach((element){
-        if(element["urlToimage"]!=null && element['description']!=null){
+        //print(element);
+        if(element["urlToImage"]!=null && element['description']!=null){
+          print("ghusssss");
           Articlemodel articlemodel=Articlemodel(
             title: element['title'],
             author:  element['author'],
@@ -24,6 +27,7 @@ class News{
         }
 
       });
+      //print(news);
     }
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -25,6 +25,7 @@ class _HomeState extends State<Home> {
     News newsClass=News();
     await newsClass.getnews();
     articles=newsClass.news;
+    //print(articles);
     setState(() {
           _loading=false;
         });
